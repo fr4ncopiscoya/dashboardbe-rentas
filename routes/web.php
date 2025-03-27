6bbe72724dc6aa5bb44c059fc3b58e3b21853da7
Franco Piscoya <francomazns@gmail.com>
2025-03-27 16:20:18 -0500

    feat(*): first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReporteController;
+use App\Http\Controllers\VCardController;
+use App\Http\Controllers\MailingController;
+use App\Http\Controllers\SubscriptionController;
+
+Route::get('/', function () {
+    return 'API REST - ERP V1';
+});
+
+Route::prefix('dashboard')->group(function () {
+    Route::post('sel-anio', [ReporteController::class, 'selAnio']);
+    Route::post('sel-mes', [ReporteController::class, 'selMes']);
+    Route::post('sel-dia', [ReporteController::class, 'selDia']);
+    Route::post('sel-contribuyente', [ReporteController::class, 'selContribuyente']);
+    Route::post('sel-concepto', [ReporteController::class, 'selConcepto']);
+    Route::post('sel-concepto-montos', [ReporteController::class, 'selConceptoMontos']);
+    Route::post('sel-tupa', [ReporteController::class, 'reporteTupa']);
+    Route::post('sel-tupa-area', [ReporteController::class, 'reporteTupaArea']);
+    Route::post('sel-tupa-area-calendar', [ReporteController::class, 'reporteTupaAreaCalendar']);
+    Route::post('sel-multa-calendar', [ReporteController::class, 'reporteMultaCalendar']);
+    Route::post('sel-dia-tupa', [ReporteController::class, 'selTupaDia']);
+});
+
+Route::prefix('v1')->group(function () {
+    Route::post('vcard-member', [VCardController::class, 'vCardMemberSearch']);
+    Route::post('contact-message', [MailingController::class, 'webSendContactMail']);
+    Route::post('add-subscriber', [SubscriptionController::class, 'addSubscriber']);
+});
